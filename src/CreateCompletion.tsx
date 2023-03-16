3282c33efb230b02beaf2a91ca2b46f6b307f843
Bin Zhang <ebnbin@gmail.com>
2023-03-16 00:43:01 -0400

    Add models for CreateCompletion

--- src/CreateCompletion.tsx
+++ src/CreateCompletion.tsx
@@ -1,6 +1,7 @@
 import React, {ChangeEvent, useState} from "react";
 import {Configuration, OpenAIApi} from "openai";
-import {Button, TextField} from "@mui/material";
+import {Button, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField} from "@mui/material";
+import {Json} from "./util";
 
 interface CreateCompletionProps {
   apiKey: string;
@@ -8,9 +9,27 @@ interface CreateCompletionProps {
 
 // https://platform.openai.com/docs/api-reference/completions/create
 export function CreateCompletion({ apiKey }: CreateCompletionProps) {
+  const modelList: string[] = [
+    'text-davinci-003',
+    'text-davinci-002',
+    'text-curie-001',
+    'text-babbage-001',
+    'text-ada-001',
+    'davinci',
+    'curie',
+    'babbage',
+    'ada',
+  ]
+
+  const [model, setModel] = useState(modelList[0]);
   const [prompt, setPrompt] = useState('');
   const [response, setResponse] = useState('');
+  const [responseData, setResponseData] = useState<any>();
   const [isLoading, setIsLoading] = useState(false);
+
+  const handleModelChange = (event: SelectChangeEvent) => {
+    setModel(event.target.value)
+  }
 
   const handlePromptChange = (event: ChangeEvent<HTMLInputElement>) => {
     setPrompt(event.target.value)
@@ -25,23 +44,40 @@ export function CreateCompletion({ apiKey }: CreateCompletionProps) {
 
   const request = async () => {
     setResponse('[Loading]')
+    setResponseData(undefined)
     setIsLoading(true)
 
     const response = await openai
       .createCompletion({
-        model: "text-davinci-003",
+        model: model,
         prompt: prompt,
       }).catch((error: any) => {
         setResponse(`[${error}]`)
+        setResponseData(error)
         setIsLoading(false)
       });
 
     setResponse(response.data.choices[0].text)
+    setResponseData(response)
     setIsLoading(false)
   }
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <FormControl fullWidth>
+        <InputLabel>{'Model'}</InputLabel>
+        <Select
+          value={model}
+          label={"Model"}
+          onChange={handleModelChange}
+        >
+          {
+            modelList.map((model) => {
+              return <MenuItem value={model}>{model}</MenuItem>
+            })
+          }
+        </Select>
+      </FormControl>
       <TextField
         label={"prompt"}
         value={prompt}
@@ -57,6 +93,9 @@ export function CreateCompletion({ apiKey }: CreateCompletionProps) {
       <div>
         {response}
       </div>
+      <Json
+        value={responseData}
+      />
     </div>
   )
 }
